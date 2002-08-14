8be2e3dcadaa1d7f769372e8f087223ab1227162
Paul Cantrell <cantrell@pobox.com>
2002-08-14 00:23:43 +0000

    added licensing; further futzing w/regex for inner classes

--- macker/src/net/innig/macker/rule/CompositePattern.java
+++ macker/src/net/innig/macker/rule/CompositePattern.java
@@ -1,3 +1,23 @@
+/*______________________________________________________________________________
+ *
+ * Current distribution and futher info:  http://innig.net/macker/
+ *
+ * Copyright 2002 Paul Cantrell
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1, as published
+ * by the Free Software Foundation. See the file LICENSE.html for more info.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the license for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330 / Boston, MA 02111-1307 / USA.
+ *______________________________________________________________________________
+ */
+ 
 package net.innig.macker.rule;
 
 import net.innig.macker.structure.ClassInfo;
