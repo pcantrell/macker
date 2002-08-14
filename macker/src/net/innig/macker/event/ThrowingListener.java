326887d3014d9d90a253e311257f91020585fc00
Paul Cantrell <cantrell@pobox.com>
2002-08-14 07:56:25 +0000

    oops ... jclasslib requires GPL

--- macker/src/net/innig/macker/event/ThrowingListener.java
+++ macker/src/net/innig/macker/event/ThrowingListener.java
@@ -5,16 +5,16 @@
  * Copyright 2002 Paul Cantrell
  * 
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License version 2.1, as published
- * by the Free Software Foundation. See the file LICENSE.html for more info.
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation. See the file LICENSE.html for more information.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the license for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation, Inc.
- * 59 Temple Place, Suite 330 / Boston, MA 02111-1307 / USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+ * Place, Suite 330 / Boston, MA 02111-1307 / USA.
  *______________________________________________________________________________
  */
  
