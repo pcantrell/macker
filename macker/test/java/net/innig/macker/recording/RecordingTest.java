b2ce585b8981744d471d8eb1a4b283a1a80d3227
Paul Cantrell <cantrell@pobox.com>
2006-01-16 00:24:08 +0000

    macker.jar now in dist root added basic no-explosion test for ant task (more work needed!)

--- macker/test/java/net/innig/macker/recording/RecordingTest.java
+++ macker/test/java/net/innig/macker/recording/RecordingTest.java
@@ -39,7 +39,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.AssertionFailedError;
